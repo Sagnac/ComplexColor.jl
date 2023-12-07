0f78235809e41c103921acac45a802f3f6c8541e
Sagnac <83491030+Sagnac@users.noreply.github.com>
2023-12-07 19:58:58 +0000

    Integrate the plotting into the package
    Might as well. This does bring in the heavy GLMakie dependency now,
    but that's okay. I still can't figure out the recipe system when it's
    not just the plot that needs to be defined, but also a specific format
    for the figure and axis, etc. Apparently their recipe system is still
    subject to change as well (maybe I'm wrong about this), and MakieCore
    doesn't bring in Colorbar, etc. either so maybe it's best to just do
    this for now.
    
    It's more convenient to have the plotting upfront and accessible while
    controlling the dependency's version rather than defining a specific
    recipe and requiring the user manage it. Whenever I figure out recipes
    I'll just change it.
    
    Also, I could just use Plots.jl or another lighter package since right
    now there's no interactivity required, but I like Makie and since I'm
    more familiar with manipulating elements in it I'm just going to go with
    that; it's more robust anyway from my experience and nowadays with newer
    versions of Julia and Makie loading it is very fast.

--- README.md
+++ README.md
@@ -2,39 +2,23 @@
 
 `complex_color(A)` converts an array of complex numbers into an image matrix of RGB values using a hue-lightness color mapping for the phase and modulus.
 
-# Installation
+`complex_plot(x, y, s; title)` plots a complex number array `s` within the `x` and `y` limits using domain coloring in the HSL color space.
+
+## Installation
 
 ```julia
 using Pkg
 Pkg.add(url="https://github.com/Sagnac/ComplexColor.jl")
 ```
 
-# Plotting example
+## Plotting example
 
 ```julia
 using ComplexColor
-using GLMakie # assumes previous installation of this plotting package
-
 # construct the complex array
-lims = -2:0.01:2
-z = [complex(x, y) for x in lims, y in lims]
+x = y = -2:0.01:2
+z = complex.(x, y')
 s = map(z -> 1/z, z)
 # transform the complex array into an image
-color_matrix = complex_color(s)
-# plot
-nticks = 5
-ticklabels = Makie.latexstring.(range(lims[begin], lims[end], nticks))
-xticks = yticks = (range(1, length(lims), nticks), ticklabels)
-arg_ticks = (-π:π/2:π, [L"-\pi", L"-\frac{\pi}{2}", L"0", L"\frac{\pi}{2}", L"\pi"])
-fig = Figure()
-axis = Axis(fig[1,1]; title = L"s = z^{-1}", titlesize = 21,
-            xlabel = L"Re(z)", xlabelsize = 16, ylabel = L"Im(z)", ylabelsize = 16,
-            xticks, yticks)
-plot = image!(axis, color_matrix)
-grid = GridLayout(fig[1,2]; tellheight = false)
-Colorbar(grid[1,1]; colormap = :gray1, ticks = (0:1, [L"0", L"\infty"]))
-Label(grid[2,1], L"|s|"; fontsize = 15)
-Colorbar(grid[1,2]; colormap = hsl, limits = (-π, π), ticks = arg_ticks)
-Label(grid[2,2], L"Arg(s)"; fontsize = 15)
-fig
+fig = complex_plot(x, y, s; title = L"s = z^{-1}")
 ```
--- src/ComplexColor.jl
+++ src/ComplexColor.jl
@@ -1,8 +1,14 @@
 module ComplexColor
 
-export complex_color, hsl
+export complex_color, complex_plot, hsl, latexstring, var"@L_str"
 
 using Images
+using GLMakie
+using .Makie: Axis, latexstring
+
+const Lims = Union{AbstractRange, Tuple, AbstractVector}
+const ComplexArray = AbstractArray{<:Complex{<:Real}}
+const Str = Union{AbstractString, Makie.LaTeXString}
 
 """
     complex_color(A)
@@ -22,13 +28,42 @@ julia> complex_color(z)
  RGB{Float64}(1.0,0.0,1.0)  RGB{Float64}(1.0,0.5,0.0)
 ```
 """
-function complex_color(A::AbstractArray{<:Complex{<:Real}})
+function complex_color(A::ComplexArray)
     r = abs.(A)
     ϕ = angle.(A)
     H = @. rad2deg(mod(ϕ + 2π/3, 2π))
     L = @. 2atan(r)/π
     S = ones(eltype(H), size(H))
     clamp01nan1!(map(RGB, HSL.(H, S, L)))
+end
+
+"""
+    complex_plot(x, y, s; [title])
+
+Plot a complex number array `s` within the `x` and `y` limits using domain coloring in the HSL color space.
+"""
+function complex_plot(x::Lims, y::Lims, s::ComplexArray; title::Str = L"s")
+    color_matrix = complex_color(s)
+    xlen, ylen = size(s)
+    nticks = 5
+    xticklabels = latexstring.(range(x[begin], x[end], nticks))
+    yticklabels = latexstring.(range(y[begin], y[end], nticks))
+    xticks = (range(1, xlen, nticks), xticklabels)
+    yticks = (range(1, ylen, nticks), yticklabels)
+    arg_tick_labels = [L"-\pi", L"-\frac{\pi}{2}", L"0", L"\frac{\pi}{2}", L"\pi"]
+    arg_ticks = (-π:π/2:π, arg_tick_labels)
+    fig = Figure()
+    axis = Axis(fig[1,1]; title, titlesize = 21,
+                xlabel = L"Re(z)", xlabelsize = 16,
+                ylabel = L"Im(z)", ylabelsize = 16,
+                xticks, yticks)
+    plot = image!(axis, color_matrix)
+    grid = GridLayout(fig[1,2]; tellheight = false)
+    Colorbar(grid[1,1]; colormap = :gray1, ticks = (0:1, [L"0", L"\infty"]))
+    Label(grid[2,1], L"|s|"; fontsize = 15)
+    Colorbar(grid[1,2]; colormap = hsl, limits = (-π, π), ticks = arg_ticks)
+    Label(grid[2,2], L"Arg(s)"; fontsize = 15)
+    fig
 end
 
 function clamp01nan1!(A)
