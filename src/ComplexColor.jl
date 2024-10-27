385870a539d3a878138a735b48d104ae18400c50
Sagnac <83491030+Sagnac@users.noreply.github.com>
2024-10-27 00:58:36 +0000

    Depend upon the new version of `Colors`
    IntervalSets will be left in `sources` for ease of reproducing the
    environment in case there's a desire to use the other range methods, but
    they aren't strictly necessary to use the package.
    
    ComplexColor does export OpenInterval and the interval string macro, but
    these aren't necessary to plot since support for the other half-open and
    closed intervals is there.

--- src/ComplexColor.jl
+++ src/ComplexColor.jl
@@ -22,11 +22,7 @@ const n = 1000
 
 struct Septaphase end
 
-const colors_compat = pkgversion(Colors) <= v"0.12.11"
-
-const default = colors_compat ? HSL : Oklch
-
-__init__() = colors_compat && @warn "Oklch color is not available in this version."
+const default = Oklch
 
 """
     complex_color(s [, color = `$default`])
@@ -248,9 +244,6 @@ end
 
 const colormaps = Dict{Spaces, Vector{RGB{Float64}}}(
     HSL   => map(RGB, HSL(i, 1.0, 0.5) for i = range(-60, 300, 2^10)),
-)
-
-colors_compat || push!(colormaps,
     Oklch => map(RGB, Oklch(0.5, chroma, i) for i = range(-30, 330, 2^10))
 )
 
